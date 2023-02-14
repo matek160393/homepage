3d4a9b879814ceba2e938e3692b39036c0a3281a
Mateusz Grincewicz <matek160393@gmail.com>
2023-02-14 11:36:07 +0100

    separation of functions

--- js/script.js
+++ js/script.js
@@ -3,35 +3,36 @@
         const photos = document.querySelector(".js-photos");
         photos.classList.toggle("article__photos--hidden");
         const photosButtonText = document.querySelector(".js-photosButtonText");
-        buttonPhotos.innerText = photos.classList.contains("article__photos--hidden")
+        photosButtonText.innerText = photos.classList.contains("article__photos--hidden")
             ? "Wyświetl moje fotografie"
             : "Ukryj fotografie";
     }
 
-    const onPhotosDisplay = () => {
-        const buttonPhotos = document.querySelector(".js-buttonPhotos");
-        buttonPhotos.addEventListener("click", togglePhotosView);
-    };
-    onPhotosDisplay();
-
     const toggleDrawingsView = () => {
         const drawings = document.querySelector(".js-drawings");
         drawings.classList.toggle("article__drawings--hidden");
-        
-        const buttonText = document.querySelector(".js-drawingsButtonText");
-        buttonText.innerText = drawings.classList.contains(".js-drawingsButtonText")
+        const drawingsButtonText = document.querySelector(".js-drawingsButtonText");
+        drawingsButtonText.innerText = drawings.classList.contains("article__drawings--hidden")
             ? "Wyświetl moje rysunki"
             : "Ukryj rysunki";
     }
 
-    const onDrawingsDisplay = () => {
+    const init = () => {
+
+        const welcome = () => {
+            console.log("Cześć!");
+        }
+        welcome();
+
         const buttonDrawings = document.querySelector(".js-buttonDrawings");
         buttonDrawings.addEventListener("click", toggleDrawingsView);
-    }
-    onDrawingsDisplay();
 
-    const welcome = () => {
-        console.log("Cześć!");
+        const buttonPhotos = document.querySelector(".js-buttonPhotos");
+        buttonPhotos.addEventListener("click", togglePhotosView);
+
     }
-    welcome();
+    init();
+
+    
+   
 };
